efb0674ee405012f6497bd82ed92a1d7c13fb4b1
yyy5044 <48915312+yyy5044@users.noreply.github.com>
2026-02-08 18:03:55 +0900

    [Silver III] Title: N과 M (4), Time: 116 ms, Memory: 16304 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		numbers = new int[M];
+		
+		comb(0, 0);
+		System.out.println(sb);
+	}
+	
+	public static void comb(int depth, int start) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(numbers[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = start; i < N; i++) {
+			numbers[depth] = i + 1;
+			comb(depth+1, i);
+		}
+	}
+
+}
