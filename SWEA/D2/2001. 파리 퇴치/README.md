9405b95dedd99a3474b5c80e297845a27c108f97
yyy5044 <48915312+yyy5044@users.noreply.github.com>
2026-02-11 21:24:25 +0900

    [D2] Title: 파리 퇴치, Time: 83 ms, Memory: 25,344 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/README.md
@@ -0,0 +1,15 @@
+# [D2] 파리 퇴치 - 2001 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PzOCKAigDFAUq) 
+
+### 성능 요약
+
+메모리: 25,344 KB, 시간: 83 ms, 코드길이: 1,417 Bytes
+
+### 제출 일자
+
+2026-02-11 21:24
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine()); // 첫 번째 줄
+		
+		for (int t = 0; t < T; t++) {
+			String line = br.readLine(); // 두 번째 줄
+			StringTokenizer st = new StringTokenizer(line);
+			
+			int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+			// System.out.println(N + ", " + M);
+			
+			int[][] map = new int[N + 1][N + 1]; // 상, 좌 패딩
+			int[][] psum = new int[N + 1][N + 1]; // 누적합
+			
+			for (int i = 1; i < N + 1; i++) {
+				line = br.readLine();
+				st = new StringTokenizer(line);
+				for (int j = 1; j < N + 1; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			
+			for (int i = 1; i < N + 1; i++) {
+				for (int j = 1; j < N + 1; j++) {
+					psum[i][j] = psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1] + map[i][j];
+				}
+			}
+			
+			int max = 0;
+			for (int i = M; i < N + 1; i++) {
+				for (int j = M; j < N + 1; j++) {
+					if (max < psum[i][j] - psum[i - M][j] - psum[i][j - M] + psum[i - M][j - M]) {
+						max = psum[i][j] - psum[i - M][j] - psum[i][j - M] + psum[i - M][j - M];
+					}
+				}
+			}
+			
+			sb.append("#"+(t+1)+" "+max).append("\n");
+		}	
+		System.out.println(sb);
+	}
+
+}
