014024c9345d185fa0b381af4623a1c8bedb4a9d
yyy5044 <48915312+yyy5044@users.noreply.github.com>
2026-02-11 21:25:22 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 78 ms, Memory: 25,600 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		for (int t = 0; t < 10; t++) {	
+			String line = br.readLine(); // 첫 번째 줄
+			//System.out.println(line);
+			
+			StringTokenizer st = new StringTokenizer(line);
+			int N = Integer.parseInt(st.nextToken());
+			
+			line = br.readLine(); // 두 번째 줄
+			//System.out.println(line);
+			st = new StringTokenizer(line);
+			
+			LinkedList<String> list = new LinkedList<>();
+			
+			for (int i = 0; i < N; i++) {
+				list.add(st.nextToken());
+			}
+			
+			
+			line = br.readLine(); // 세 번째 줄
+			//System.out.println(line);
+			int M = Integer.parseInt(line);
+			
+			line = br.readLine(); // 네 번째 줄
+			//System.out.println(line);
+			st = new StringTokenizer(line);
+			
+			for (int i = 0; i < M; i++) {
+				String instruct = st.nextToken();
+				
+				if (instruct.equals("I")) {
+					int x = Integer.parseInt(st.nextToken());
+					int y = Integer.parseInt(st.nextToken());
+					
+					String[] s = new String[y];
+					for (int j = 0; j < y; j++) {
+						s[j] = st.nextToken();
+					}
+					
+					for (int j = 0; j < y; j++) {
+						list.add(x + j, s[j]);
+					}
+				}
+				
+				else if (instruct.equals("D")) {
+					int x = Integer.parseInt(st.nextToken());
+					int y = Integer.parseInt(st.nextToken());
+					
+					for (int j = 0; j < y; j++) {
+						if (x == 0) {
+							list.remove(0);
+						} else {
+							list.remove(x);
+						}				
+					}
+				}
+			}
+			
+			sb.append("#" + (t + 1) + " ");
+			for (int i = 0; i < 10; i++) {
+				sb.append(list.get(i) + " ");
+			}
+			sb.append("\n");
+			
+		}
+		System.out.println(sb);
+		
+	}
+}
