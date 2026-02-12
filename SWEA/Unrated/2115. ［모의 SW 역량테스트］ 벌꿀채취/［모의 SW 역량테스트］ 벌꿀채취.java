6a1da607ffbdddce448c4c3840fc724058ee45da
yyy5044 <48915312+yyy5044@users.noreply.github.com>
2026-02-12 16:07:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 101 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -1,29 +1,26 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Solution {
-	static int[][] map;
-	static int[][] mapMax;
-	static int[] window;
-	static int N;
-	static int M;
-	static int C;
+	static int N, M, C;
+	static int[][] map, mapMax;
+	static int[] num;
+	static ArrayList<Box> list = new ArrayList<>();
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
 		
+		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			C = Integer.parseInt(st.nextToken());
 			map = new int[N][N];
-			mapMax = new int[N][N - M + 1];
-			window = new int[M];
-			
-			for (int i = 0; i < N; i++) {
+			mapMax = new int[N][N-M+1];
+			num = new int[M];
+			for(int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
@@ -32,50 +29,66 @@ public class Solution {
 			
 			for (int r = 0; r < N; r++) {
 				for (int i = 0; i < N-M+1; i++) {
-					
-					for (int j = i, windowIdx = 0; j < M + i; j++, windowIdx++) {
-						window[windowIdx] = map[r][j];
+					for (int j = i, idx=0; j < M+i;j++) {
+						num[idx++] = map[r][j];
 					}
 					subset(0, 0, 0, r, i);
+					list.add(new Box(r, i, mapMax[r][i]));
 				}
 			}
 			
-			int maxProfit = 0;
-			for (int r1 = 0; r1 < N; r1++) {
-				for (int i1 = 0; i1 < N-M+1; i1++) {
-					for (int r2 = r1; r2 < N; r2++) {
-						int start = (r1==r2)? (i1+(N-M+1)) : 0;
-						for (int i2 = start; i2 < N-M+1; i2++) {
-							if (maxProfit < mapMax[r1][i1] + mapMax[r2][i2]) {
-								maxProfit = mapMax[r1][i1] + mapMax[r2][i2];
+			int maxProfit = Integer.MIN_VALUE;
+			for (int i = 0; i < list.size(); i++) {
+				for (int j = i+1; j < list.size(); j++) {
+					if (list.get(i).r == list.get(j).r) {
+						if (list.get(i).c + M <= list.get(j).c) {
+							int totalProfit = list.get(i).maxProfit + list.get(j).maxProfit;
+							if (totalProfit > maxProfit) {
+								maxProfit = totalProfit;
 							}
 						}
+					} else {
+						int totalProfit = list.get(i).maxProfit + list.get(j).maxProfit;
+						if (totalProfit > maxProfit) {
+							maxProfit = totalProfit;
+						}
 					}
-
 				}
 			}
 			
-			sb.append("#").append(t+1).append(" ").append(maxProfit).append("\n");
-
-			// for (int i = 0; i < N; i++) System.out.println(Arrays.toString(mapMax[i]));
+			sb.append("#").append(t+1).append(" ")
+				.append(maxProfit).append("\n");
+			N = M = C = 0;
+			list.clear();
 		}
-		
-
 		
 		System.out.println(sb);
 	}
 	
-	public static void subset(int depth, int sum, int powSum, int i, int j) {
+	public static void subset(int depth, int sum, int powSum, int r, int c) {
+		if (sum>C) return;
 		
-		if (depth == M) {
-			if (mapMax[i][j] < powSum && sum <= C) {
-				mapMax[i][j] = powSum;
+		if(depth==M) {
+			if (mapMax[r][c] < powSum) {
+				mapMax[r][c] = powSum;
 			}
 			return;
 		}
 		
-		subset(depth + 1, sum + window[depth], powSum + (window[depth]*window[depth]), i, j);
-		subset(depth + 1, sum, powSum, i, j);
-		
+		subset(depth+1, sum+num[depth], powSum+(num[depth]*num[depth]), r, c);
+		subset(depth+1, sum, powSum, r, c);
+	}
+	
+	
+}
+
+
+class Box{
+	int r, c, maxProfit;
+	public Box(int r, int c, int maxProfit) {
+		super();
+		this.r = r;
+		this.c = c;
+		this.maxProfit = maxProfit;
 	}
 }
