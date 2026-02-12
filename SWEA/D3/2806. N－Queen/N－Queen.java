1048944974640751972f01efce299697ffc94ab9
yyy5044 <48915312+yyy5044@users.noreply.github.com>
2026-02-12 13:53:20 +0900

    [D3] Title: N-Queen, Time: 97 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -4,13 +4,12 @@ import java.util.*;
 public class Solution {
 	static int N, count = 0;
 	static boolean[] col, slash, bSlash;
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
 		int T = Integer.parseInt(br.readLine());
-		
 		for (int t = 0; t < T; t++) {
 			N = Integer.parseInt(br.readLine());
 			col = new boolean[N];
@@ -18,35 +17,31 @@ public class Solution {
 			bSlash = new boolean[2*N];
 			
 			setQueen(0);
-
+			
 			sb.append("#").append(t+1).append(" ")
 				.append(count).append("\n");
-			
-			count = 0;
 			N = 0;
+			count = 0;
 		}
 		
 		System.out.println(sb);
 	}
 	
 	public static void setQueen(int r) {
-		// 종료조건
-		if (r == N) {
-			count++;
+		
+		if(r == N) { // 끝까지 오면 경우의 수를 발견한 것
+			count++; // 경우의 수
 			return;
 		}
-
-		// 해당 행의 모든 열에 퀸 둬보기
-		for (int i = 0; i < N; i++) {
-			// 가지치기
-			if (col[i] || slash[r+i] || bSlash[(r-i)+N]) continue;
-			
-			col[i] = slash[r+i] = bSlash[(r-i)+N] = true;
-			setQueen(r+1);
-			col[i] = slash[r+i] = bSlash[(r-i)+N] = false;
-		}
 		
+		for (int c = 0; c < N; c++) { // 모든 열에 시도
+			if (col[c] || slash[r+c] || bSlash[r-c+N]) {
+				continue; // 못 놓으면 다른 열 시도
+			}
+			col[c] = slash[r+c] = bSlash[r-c+N] = true;
+			setQueen(r+1);
+			col[c] = slash[r+c] = bSlash[r-c+N] = false;
+		}
 	}
 	
-
 }
