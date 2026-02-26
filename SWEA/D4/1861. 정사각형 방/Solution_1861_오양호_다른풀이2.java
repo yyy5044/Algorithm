6b46c6413d8b7fada5dfd51602d7c156525853db
HaPo <yyy5044@naver.com>
2026-02-26 10:02:26 +0900

    rename

--- SWEA/D4/1861. 정사각형 방/Solution_1861_오양호_다른풀이2.java
+++ SWEA/D4/1861. 정사각형 방/Solution_1861_오양호_다른풀이2.java
@@ -1,74 +1,79 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 
 public class Solution {
 	static int N;
-	static int[][] map, memo;
+	static int[][] map;
+	static boolean[][] visited;
 	static int[] dr = {-1,1,0,0}, dc = {0,0,-1,1};
 	
-	static int[] maxRoom = new int[] {Integer.MIN_VALUE, -1};
-	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		int T = Integer.parseInt(br.readLine());	
-		for (int t = 0; t < T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			
+		int T = Integer.parseInt(br.readLine());
+		for(int t = 1; t <= T; t++) {
+			N = Integer.parseInt(br.readLine());
 			map = new int[N][N];
-			memo = new int[N][N]; // 메모이제이션 배열
-			for (int r = 0; r < N; r++) {
-				st= new StringTokenizer(br.readLine());
-				for (int c = 0; c < N; c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
+			visited = new boolean[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
+			
+			int maxMove = 0;
+			int roomNum = 0;
 			for (int r = 0; r < N; r++) {
 				for (int c = 0; c < N; c++) {
-					memo[r][c] = dfs(r, c);
-					if(maxRoom[0] == memo[r][c]) {
-						maxRoom[1] = Math.min(maxRoom[1], map[r][c]);
-					} else if (maxRoom[0] < memo[r][c]) {
-						maxRoom[0] = memo[r][c];
-						maxRoom[1] = map[r][c];
+					int[] start = {r,c,1};
+					int move = bfs(start);
+					if (move == maxMove) { // 이동횟수가 같으면 더 작은 방 번호 기록
+						roomNum = Math.min(roomNum, map[r][c]);
+					} else if (move>maxMove){
+						maxMove = move;
+						roomNum = map[r][c];
 					}
 				}
 			}
 			
-			sb.append("#").append(t+1).append(" ")
-				.append(maxRoom[1]).append(" ").append(maxRoom[0]).append("\n");
-			
-			maxRoom[0] = Integer.MIN_VALUE;
-			maxRoom[1] = -1;
-			N = 0;
+			sb.append("#").append(t).append(" ")
+				.append(roomNum+" "+maxMove).append("\n");
 		}
 		
 		System.out.println(sb);
 	}
-	
-	public static int dfs(int r, int c) {
-		if (memo[r][c] != 0) return memo[r][c]; // 이전에 계산되었다면 계산된 값 주고 바로 종료
+
+	private static int bfs(int[] start) {
+		int maxCnt = 0;
 		
-		memo[r][c] += 1; // 자기 방 개수 기록
+		ArrayDeque<int[]> dq = new ArrayDeque<>();
+		visited[start[0]][start[1]] = true;
+		dq.addLast(start);
 		
-		for (int d = 0; d < 4; d++) { // 들어갈 수 있는 방 탐색
-			int nr = r + dr[d];
-			int nc = c + dc[d];
+		while(!dq.isEmpty()) {
+			int[] cur = dq.pollFirst();
+			int r = cur[0];
+			int c = cur[1];
+			int cnt = cur[2];
 			
-			if (nr >= 0 && nc >= 0 && nr < N && nc < N) {
-				if (map[nr][nc] == map[r][c]+1) {
-					memo[r][c] += dfs(nr, nc); // +1은 자기 방 개수 더한 거
-                    break; // 들어갈 수 있는 방은 단 한 개이므로 찾았으면 다른 시도를 차단
+			maxCnt = Math.max(maxCnt, cnt);
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+				
+				if (nr < 0 || nc < 0 || nr > N-1 || nc > N-1) continue; // 방이 없을 때 스킵
+				if (map[nr][nc] == map[r][c]+1) { // 다음 방 번호가 현재 방 번호보다 딱 1만큼 클 때 입장
+					visited[nr][nc] = true;
+					dq.addLast(new int[] {nr, nc, cnt+1}); // 다음 방으로 입장, 카운트 하나 늘리기
 				}
 			}
 		}
 		
-		return memo[r][c];
-	}
-
-	
+		return maxCnt;
+	} 
 }
