0ee74bfddfc7fda6c46b4cfcaf3e1b00885f1762
hapo <yyy5044@naver.com>
2026-04-10 23:46:31 +0900

    rename

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/Main_11053_오양호.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine().trim());
+		
+		int[] a = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] dp = new int[n];
+		Arrays.fill(dp, 1); // initialize
+		
+		int ans = 1;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (a[j] < a[i]) {
+					dp[i] = Math.max(dp[i], dp[j]+1);
+				}
+			}
+			ans = Math.max(ans, dp[i]);
+		}
+		
+		System.out.println(ans);
+	}
+
+}
