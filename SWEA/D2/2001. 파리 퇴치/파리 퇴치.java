e46918523384bead785afbf55e6866e8f0bd8329
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-07-09 21:38:31 +0900

    [D2] Title: 파리 퇴치, Time: 72 ms, Memory: 24,064 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -2,50 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	public static void main(String[] args) throws IOException{
-		
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine()); // 첫 번째 줄
-		
-		for (int t = 0; t < T; t++) {
-			String line = br.readLine(); // 두 번째 줄
-			StringTokenizer st = new StringTokenizer(line);
-			
-			int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
-			// System.out.println(N + ", " + M);
-			
-			int[][] map = new int[N + 1][N + 1]; // 상, 좌 패딩
-			int[][] psum = new int[N + 1][N + 1]; // 누적합
-			
-			for (int i = 1; i < N + 1; i++) {
-				line = br.readLine();
-				st = new StringTokenizer(line);
-				for (int j = 1; j < N + 1; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			
-			for (int i = 1; i < N + 1; i++) {
-				for (int j = 1; j < N + 1; j++) {
-					psum[i][j] = psum[i - 1][j] + psum[i][j - 1] - psum[i - 1][j - 1] + map[i][j];
-				}
-			}
-			
-			int max = 0;
-			for (int i = M; i < N + 1; i++) {
-				for (int j = M; j < N + 1; j++) {
-					if (max < psum[i][j] - psum[i - M][j] - psum[i][j - M] + psum[i - M][j - M]) {
-						max = psum[i][j] - psum[i - M][j] - psum[i][j - M] + psum[i - M][j - M];
-					}
-				}
-			}
-			
-			sb.append("#"+(t+1)+" "+max).append("\n");
-		}	
-		System.out.println(sb);
-	}
 
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			int[][] S = new int[n+1][n+1];
+			
+			for (int i = 1; i < n+1; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 1; j < n+1; j++) {
+					S[i][j] = Integer.parseInt(st.nextToken()) + S[i-1][j] + S[i][j-1] - S[i-1][j-1];
+				}
+			}
+			
+			int max = Integer.MIN_VALUE;
+			for (int i = m; i < n+1; i++) {
+				for (int j = m; j < n+1; j++) {
+					int sum = 0;
+					sum = S[i][j] - S[i-m][j] - S[i][j-m] + S[i-m][j-m];
+					max = Math.max(max, sum);
+				}
+			}
+			
+			sb.append("#").append(t).append(" ");
+			sb.append(max).append("\n");
+			
+		}
+
+		System.out.println(sb);
+
+	}
+	
 }
