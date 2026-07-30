26658a04cc5e4df0126d9b4cd2eaa8827bc5582f
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-07-30 23:47:45 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -1,6 +1,6 @@
 select BOOK_ID, AUTHOR_NAME, PUBLISHED_DATE
-from BOOK as b
-join AUTHOR as a
-on b.AUTHOR_ID = a.AUTHOR_ID
-where CATEGORY = '경제'
-order by PUBLISHED_DATE;
+from BOOK 
+join AUTHOR
+on BOOK.AUTHOR_ID = AUTHOR.AUTHOR_ID
+where BOOK.CATEGORY = '경제'
+order by PUBLISHED_DATE asc;
