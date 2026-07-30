7d2bbd7933e5a12a45557a29fc07a33e38a058c9
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-07-31 08:49:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.11 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,8 +1,9 @@
-import java.util.HashMap;
+import java.util.*;
+import java.io.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap <String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         
         for (String c : completion) {
             map.put(c, map.getOrDefault(c, 0) + 1);
@@ -11,7 +12,7 @@ class Solution {
         for (String p : participant) {
             int count = map.getOrDefault(p, 0);
             if (count == 0) return p;
-            map.put(p, count-1);
+            map.put(p, count - 1);
         }
         
         return "";
