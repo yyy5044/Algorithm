b97cebbc4d2942da5796b5742a6bb57b0911093e
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-07-30 22:39:32 +0900

    [level 1] Title: 폰켓몬, Time: 1.69 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,19 +3,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
         
         for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
+            set.add(n);
         }
         
-        int size = map.size();
-        int N = nums.length;
-        
-        if (size >= N/2) { // 모든 포켓몬을 다르게 할 수 있는 경우
-            return N/2;
-        } else { // 어쩔 수 없이 중복된 포켓몬을 골라야 하는 경우
-            return size;
-        }
+        return Math.min(nums.length / 2, set.size());
     }
 }
