76610592df9d26204d6c3fb99ff02ed27289f321
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-07-31 19:46:47 +0900

    [level 2] Title: 타겟 넘버, Time: 4.19 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,39 +3,28 @@ import java.util.*;
 
 class Solution {
     static int ans;
-    static int[] comb;
+    static int T;
+    static int[] nums;
     
     public int solution(int[] numbers, int target) {
         ans = 0;
+        T = target;
+        nums = numbers;
         
-        int sum = 0;
-        for (int n : numbers) {
-            sum += n;
-        }
-        
-        for (int i = 0; i < numbers.length+1; i++) {
-            comb = new int[i];
-            dfs(0, 0, numbers, target, i, sum);
-        }
-        
+        dfs(0, 0);
+            
         return ans;
     }
     
-    static void dfs(int depth, int start, int[] numbers, int target, 
-                    int n, int sum) {
-        if (depth == n) {
-            for (int i = 0; i < n; i++) {
-                sum -= 2 * numbers[comb[i]];
+    static void dfs(int depth, int sum) {
+        if (depth == nums.length) {
+            if (sum == T) {
+                ans++;
             }
-            
-            if (sum == target) ans++;
-            
             return;
         }
         
-        for (int i = start; i < numbers.length; i++) {
-            comb[depth] = i;
-            dfs(depth+1, i+1, numbers, target, n, sum);
-        }
+        dfs(depth+1, sum + nums[depth]);
+        dfs(depth+1, sum - nums[depth]);
     }
 }
