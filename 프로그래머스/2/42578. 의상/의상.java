ed712fb2685d92db85e9de6bc10a44e082a09f9a
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-07-31 19:38:53 +0900

    [level 2] Title: 의상, Time: 0.58 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -5,16 +5,16 @@ class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        for (String[] c : clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
         }
         
         int ans = 1;
-        
-        for (int count : map.values()) {
-            ans *= count + 1;
+        for (int n : map.values()) {
+            ans *= n + 1;
         }
+        ans -= 1;
         
-        return ans - 1;
+        return ans;
     }
 }
