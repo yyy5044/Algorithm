790a081c010ab6942ddc63369d765d524f8c32f9
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-01 20:51:46 +0900

    [level 2] Title: DATETIME에서 DATE로 형 변환, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
+++ 프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -1,3 +1,4 @@
-select ANIMAL_ID, NAME, DATE(DATETIME) as 날짜
+-- ANIMAL_INS: 동물 보호소에 들어온 동물의 정보
+select ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, '%Y-%m-%d') as `날짜`
 from ANIMAL_INS
 order by ANIMAL_ID;
