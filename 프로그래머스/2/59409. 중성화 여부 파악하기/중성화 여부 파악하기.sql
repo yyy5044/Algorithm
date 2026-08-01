4832ba508a59c66ff27011d2f7437b75aab16b29
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-01 20:34:27 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,6 +1,8 @@
-select ANIMAL_ID, NAME, case
-                            when SEX_UPON_INTAKE like 'Neutered%' then 'O'
-                            when SEX_UPON_INTAKE like 'Spayed%' then 'O'
-                            else 'X'
-                        end as 중성화
-from ANIMAL_INS;
+-- ANIMAL_INS : 동물 보호소에 들어온 동물의 정보
+select ANIMAL_ID, NAME, 
+    case 
+        when (SEX_UPON_INTAKE like 'Neutered%') or (SEX_UPON_INTAKE like 'Spayed%') then 'O'
+        else 'X'
+    end as `중성화`
+from ANIMAL_INS
+order by ANIMAL_ID;
