d82429b6b19f0d1cbc2e826dcd0a16058370d3b7
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-04 00:04:11 +0900

    [level 2] Title: 가장 큰 수, Time: 99.92 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -9,12 +9,11 @@ class Solution {
             arr[i] = String.valueOf(numbers[i]);
         }
         
-        // Arrays.sort(arr, (a, b) -> Integer.parseInt(b+a) - Integer.parseInt(a+b));
         Arrays.sort(arr, (a, b) -> (b+a).compareTo(a+b));
-        
         if (arr[0].equals("0")) return "0";
         
         StringBuilder sb = new StringBuilder();
+        
         for (String s : arr) {
             sb.append(s);
         }
