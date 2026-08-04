68e791ab5b5c934e2a90f0d7d087c92ae7a5be5c
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-04 09:29:59 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,7 +1,6 @@
--- root 아이템이면 parent 아이템 id가 null임을 이용 -> 문제에서 알려주기도 하네
-select i.ITEM_ID, i.ITEM_NAME
-from ITEM_INFO as i
-join ITEM_TREE as t
-using(ITEM_ID)
-where PARENT_ITEM_ID is null
-order by ITEM_ID;
+select ITEM_ID, ITEM_NAME
+from ITEM_INFO
+join ITEM_TREE
+using (ITEM_ID)
+where PARENT_ITEM_ID IS NULL
+order by ITEM_ID asc;
