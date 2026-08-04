9a04529a40cdc4eba7324a4ffa636a1c9fb8d0f2
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-04 09:12:32 +0900

    [level 2] Title: H-Index, Time: 1.73 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,15 +4,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         Integer[] arr = new Integer[citations.length];
+        
         for (int i = 0; i < citations.length; i++) {
             arr[i] = citations[i];
         }
         
         Arrays.sort(arr, (a, b) -> b - a);
-        
+        // 6 5 3 1 0
         int h = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (i+1 <= arr[i]) {
+        for (int i = 0; i < citations.length; i++) {
+            if (arr[i] >= i+1) {
                 h = i+1;
             } else {
                 break;
