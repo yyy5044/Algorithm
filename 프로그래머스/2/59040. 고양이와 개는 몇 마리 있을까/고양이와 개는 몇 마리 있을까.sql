e766e4e2897935ec5056ee2270b00d778974cefd
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-04 13:17:00 +0900

    [level 2] Title: 고양이와 개는 몇 마리 있을까, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
+++ 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
@@ -1,5 +1,5 @@
-select ANIMAL_TYPE, count(ANIMAL_TYPE) as count
+-- 코드를 입력하세요
+select ANIMAL_TYPE, count(*) as count
 from ANIMAL_INS
-where ANIMAL_TYPE = 'Dog' or ANIMAL_TYPE = 'Cat'
 group by ANIMAL_TYPE
-order by ANIMAL_TYPE asc;
+order by ANIMAL_TYPE;
