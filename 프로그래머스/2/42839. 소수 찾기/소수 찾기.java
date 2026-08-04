66f0671477f8e9939a075ea5d875119d633d229b
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-04 22:58:34 +0900

    [level 2] Title: 소수 찾기, Time: 15.11 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,56 +2,42 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    static char[] nums;
     static Set<Integer> set;
-    static char[] arr;
     
     public int solution(String numbers) {
-        // 1. 한 자리씩 떼어내서 배열에 담는다.
-        // 2. 모든 경우의 수로 붙여본다.
-        // 3. 완성이 되면 Set에 넣어본다.
-        // 4. 전부 다 보면 Set에 있는 소수의 개수를 센다.
         set = new HashSet<>();
-        
-        arr = new char[numbers.length()];
-        
+        nums = new char[numbers.length()];
         for (int i = 0; i < numbers.length(); i++) {
-            arr[i] = numbers.charAt(i);
+            nums[i] = numbers.charAt(i);
         }
         
-        for (int i = 1; i <= numbers.length(); i++) {
-            boolean[] visited = new boolean[arr.length];
-            dfs(0, i, visited, "");
+        for (int n = 1; n <= numbers.length(); n++) {
+            boolean[] visited = new boolean[numbers.length()];
+            dfs(0, n, visited, "");
         }
-        
         
         return set.size();
     }
     
-    static void dfs(int depth, int n, boolean[] visited, String num) {
+    static void dfs(int depth, int n, boolean[] visited, String b) {
         if (depth == n) {
-            int candidate = Integer.parseInt(num);
+            int number = Integer.parseInt(b);
             
-            if (candidate < 2) return;
+            if (number < 2) return;
             
-            boolean isPrime = true;
-            for (int i = 2; i < candidate; i++) {
-                if (candidate % i == 0) {
-                    isPrime = false;
-                    break;
-                }
+            for (int i = 2; (long) i*i <= number; i++) {
+                if (number % i == 0) return;
             }
             
-            if (isPrime) {
-                set.add(candidate);
-            }
-            
+            set.add(number);
             return;
         }
         
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                dfs(depth + 1, n, visited, num + arr[i]);
+                dfs(depth+1, n, visited, b+nums[i]);
                 visited[i] = false;
             }
         }
