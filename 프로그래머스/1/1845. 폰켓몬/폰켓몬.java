3c686cc99c49c6404ac3783938f909118bdc841d
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-11 17:50:09 +0900

    [level 1] Title: 폰켓몬, Time: 1.81 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,13 +4,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         Set<Integer> set = new HashSet<>();
+        int N = nums.length;
         
         for (int n : nums) {
             set.add(n);
         }
         
-        int ans = Math.min(nums.length/2, set.size());
-        
-        return ans;
+        if (set.size() >= N/2) {
+            return N/2;
+        } else {
+            return set.size();
+        }
     }
 }
