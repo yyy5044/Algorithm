cae6962c32b717af5321893ace53643baab5a82d
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-12 09:32:21 +0900

    [level 2] Title: 전화번호 목록, Time: 184.95 ms, Memory: 235 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,11 +3,17 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        Set<String> set = new HashSet<>();
         
-        for (int i = 1; i < phone_book.length; i++) {
-            if (phone_book[i].startsWith(phone_book[i-1])) {
-                return false;
+        for (int i = 0; i < phone_book.length; i++) {
+            set.add(phone_book[i]);
+        }
+        
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 1; j < phone_book[i].length(); j++) {
+                if (set.contains(phone_book[i].substring(0,j))){
+                    return false;
+                }
             }
         }
         
