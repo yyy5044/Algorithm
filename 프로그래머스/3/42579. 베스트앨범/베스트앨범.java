ddb7021cd4f4a0f44e1608724c61c2af15591b53
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-17 15:07:10 +0900

    [level 3] Title: 베스트앨범, Time: 1.40 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -8,6 +8,7 @@ class Solution {
         
         for (int i = 0; i < genres.length; i++) {
             genreTotal.put(genres[i], genreTotal.getOrDefault(genres[i], 0) + plays[i]);
+            
             if (!genreSongs.containsKey(genres[i])) {
                 genreSongs.put(genres[i], new ArrayList<int[]>());
             }
@@ -16,27 +17,22 @@ class Solution {
         
         List<Map.Entry<String, Integer>> entries = new ArrayList<>(genreTotal.entrySet());
         entries.sort((a, b) -> b.getValue() - a.getValue());
-    
         
-        List<Integer> list = new ArrayList<>();
+        List<Integer> tmp = new ArrayList<>();
         for (Map.Entry<String, Integer> e : entries) {
             String genre = e.getKey();
-            
-            List<int[]> songs = genreSongs.get(genre);
-            
-            songs.sort((a, b) -> {
+            genreSongs.get(genre).sort((a, b) -> {
                 if (a[0] != b[0]) return b[0] - a[0];
                 else return a[1] - b[1];
             });
             
-            for (int i = 0; i < Math.min(songs.size(), 2); i++) {
-                int[] song = songs.get(i);
-                list.add(song[1]);
+            for (int i = 0; i < Math.min(2, genreSongs.get(genre).size()); i++) {
+                tmp.add(genreSongs.get(genre).get(i)[1]);
             }
         }
         
-        int[] ans = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) ans[i] = list.get(i);
+        int[] ans = new int[tmp.size()];
+        for (int i = 0; i < tmp.size(); i++) ans[i] = tmp.get(i);
         
         return ans;
     }
