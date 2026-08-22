b4419745c5247b53190341e1b0fa91bf7b3310b8
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-22 16:45:38 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,17 +1,11 @@
-select sum(SCORE) as SCORE, EMP_NO, EMP_NAME, POSITION, EMAIL
-from HR_EMPLOYEES
-join HR_GRADE using(EMP_NO)
-group by EMP_NO
-having sum(SCORE) = (
-                        select max(year_total)
-                        from (
-                                select sum(SCORE) as year_total
-                                from HR_GRADE
-                                where YEAR = 2022
-                                group by EMP_NO
-                            ) sub
-                    );
+-- 코드를 작성해주세요
+SELECT A.SCORE SCORE, EMP_NO, B.EMP_NAME EMP_NAME, B.POSITION POSITION, B.EMAIL EMAIL
+FROM (
+    SELECT EMP_NO, SUM(SCORE) SCORE
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+) A
+JOIN HR_EMPLOYEES B USING (EMP_NO)
+ORDER BY SCORE DESC
+LIMIT 1;
 
-
-    
-    
