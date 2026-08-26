cea1a004efd3078c8c331c8ba0fb35a7639e4957
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-08-26 13:18:25 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 27.62 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,74 +1,66 @@
 import java.io.*;
 import java.util.*;
-// 문은 자물쇠로 잠겨있다.
-// 문 앞에는 열쇠와 자물쇠 푸는 방법이 써 있는 종이가 있다.
-// 자물쇠는 N*N 크기의 정사각 격자 형태다.
-// 열쇠는 M*M 크기인 정사각 격자 형태다.
 
 class Solution {
     public boolean solution(int[][] key, int[][] lock) {
         int N = lock.length;
         int M = key.length;
-        int P = 2*M+N-3;
+        // 1. 기준은 열쇠의 왼쪽 끝부분
+        // 2. 평행이동의 범위는 어떻게 되는가?
+        // -(M-1) ~ N-1까지 -> 0 ~ N+M-2
+        // 3. 회전 함수 만들기 -> 회전은 [i][j] = [M-1-j][i]
         
         boolean ans = false;
         for (int count = 0; count < 4; count++) {
-            int[][] rotatedKey = rotateClockwise(key, count);
-
-            for (int n = 0; n < N+M-1; n++) {
-                for (int m = 0; m < N+M-1; m++) {
-                    int[][] board = new int[P+1][P+1];
-                    
-                    for (int r = M-1; r < M-1+N; r++) {
-                        for (int c = M-1; c < M-1+N; c++) {
-                            board[r][c] = lock[r-M+1][c-M+1];
+            int[][] rotatedKey = rotation(key, count);
+            for (int r = 0; r < N+M-1; r++) {
+                for (int c = 0; c < N+M-1; c++) {
+                    int[][] padding = new int[N+2*(M-1)][N+2*(M-1)];
+                    // (r,c)가 왼쪽 끝
+                    for (int i = 0; i < M; i++) {
+                        for (int j = 0; j < M; j++) {
+                           padding[r+i][c+j] = rotatedKey[i][j];
                         }
                     }
                     
-                    for (int r = 0; r < M; r++) {
-                        for (int c = 0; c < M; c++) {
-                            board[n+r][m+c] += rotatedKey[r][c];
+                    // 중앙에 자물쇠 그리기
+                    for (int i = 0; i < N; i++) {
+                        for (int j = 0; j < N; j++) {
+                            padding[M-1+i][M-1+j] += lock[i][j];
                         }
                     }
                     
-                    boolean flag = true;
-                    for (int r = M-1; r < M-1+N; r++) {
-                        for (int c = M-1; c < M-1+N; c++) {
-                            if (board[r][c] != 1) {
-                                flag = false;
-                                break;
-                            }
+                    // 딱 맞는지 검사
+                    int sum = 0;
+                    for (int i = 0; i < N; i++) {
+                        for (int j = 0; j < N; j++) {
+                            if (padding[M-1+i][M-1+j] == 1) sum++;
                         }
-                        if (!flag) break;
                     }
                     
-                    if (flag) {
-                        ans = true;
-                        break;
-                    } 
+                    if (sum == N*N) return true;
                 }
-                if (ans) break;
             }
-            
         }
         
-        return ans;
+        return false;
     }
     
-    
-    static int[][] rotateClockwise(int[][] arr, int count) {
-        int n = arr.length;
-        int[][] cur = arr;
+    static int[][] rotation(int[][] key, int count){
+        int M = key.length;
+        int[][] result = key;
+        int[][] tmp = key;
         for (int c = 0; c < count; c++) {
-            int[][] next = new int[n][n];
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr.length; j++) {
-                    next[i][j] = cur[n-1-j][i];
+            result = new int[M][M];
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < M; j++) {
+                    result[i][j] = tmp[M-1-j][i];
                 }
             }
-            cur = next;
+            tmp = result;
         }
-
-        return cur;
+        
+        
+        return result;
     }
 }
