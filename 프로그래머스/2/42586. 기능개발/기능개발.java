b704aedf244ea0a42018bcf6bf9c65765ca28dee
HaPo <48915312+yyy5044@users.noreply.github.com>
2026-09-02 17:39:50 +0900

    [level 2] Title: 기능개발, Time: 0.24 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,42 +1,46 @@
 import java.io.*;
 import java.util.*;
-// 배포는 선행 기능이 배포 되어야지만 배포될 수 있음
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // progresses: 작업 진행률. (배포되어야 하는 순서대로 저장되어 있음)
-        // speeds: 각 작업의 개발 속도
-        // 중요: 배포는 하루에 한 번만 가능
-        Deque<int[]> dq = new ArrayDeque<>();
-        for (int i = 0; i < progresses.length; i++) {
+        int n = progresses.length;
+        int[] days = new int[n];
+        for (int i = 0; i < n; i++) {
+            int count = 0;
             int progress = progresses[i];
             int speed = speeds[i];
-            
-            dq.add(new int[]{i, progress, speed});
-        }
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        while(!dq.isEmpty()) {
-            for (int i = 0; i < dq.size(); i++) {
-                int[] tmp = dq.poll();
-                int idx = tmp[0], progress = tmp[1], speed = tmp[2];
-                dq.add(new int[]{i, progress + speed, speed});
+            while (progress < 100) {
+                progress += speed;
+                count++;
             }
             
-            int count = 0;
+            days[i] = count;
+        }
+        
+        Queue<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            dq.add(days[i]);
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        while(!dq.isEmpty()) {
+            int cur = dq.poll();
+            int count = 1;
             while(!dq.isEmpty()) {
-                int[] cur = dq.peek();
-                if (cur[1] < 100) break; 
+                int next = dq.peek();
+                if (cur < next) break;
                 dq.poll();
                 count++;
             }
-            if (count != 0) list.add(count);
+            
+            list.add(count);
         }
         
-        int[] arr = new int[list.size()];
+        int[] ans = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
-            arr[i] = list.get(i);
+            ans[i] = list.get(i);
         }
         
-        return arr;
+        return ans;
     }
 }
